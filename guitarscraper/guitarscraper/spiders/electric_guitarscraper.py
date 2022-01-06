e029f1472383ef70d83d111fa4d65be0ef3335ac
Adam Swartz <96804935+adamswartz4@users.noreply.github.com>
2022-01-05 22:00:31 -0800

    Add files via upload

---
+++ guitarscraper/guitarscraper/spiders/electric_guitarscraper.py
@@ -0,0 +1,18 @@
+import scrapy
+class GuitarScraper(scrapy.Spider):
+    
+    name= 'electric'
+    start_urls = ['https://www.long-mcquade.com/?page=departments&DepartmentsID=12&StockFilter=0&OnSaleOnly=1']
+
+    def parse(self, response):
+        count=-1
+        for products in response.css('div.products-item '):
+            count+=1
+            yield{
+                'name': products.css('span::text').get(),  
+                'model': products.css('span::text')[1].get(),
+                'brand': products.css('h5::text').get(),
+                'sale price': products.css('span.font-red::text').get(),
+                'original price':products.css('p.products-item-price::text').get(), 
+                'link': response.css('a.products-item-link')[count].attrib['href']
+                }
